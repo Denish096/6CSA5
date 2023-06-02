06dd20e313df38738938cc97aa36076f5d77d95c
denis <denis@Denish>
2023-06-02 22:57:11 +0530

    This is my 4th commit

---
+++ JSP/src/SearchContext/Find_elements.java
@@ -0,0 +1,16 @@
+package SearchContext;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Find_elements {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.fb.com");
+		driver.manage().window().maximize();
+		driver.findElements(By.tagName("input"));
+		System.out.println("elements identified");
+		driver.close();
+	}
+}
